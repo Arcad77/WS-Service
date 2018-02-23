119ab33f99ebef2dba059829d20cc38299a2a915
Arcad77 <36656974+Arcad77@users.noreply.github.com>
2018-02-23 22:56:04 +0500

    Update ws-client.java

--- src/wsClient/ws-client.java
+++ src/wsClient/ws-client.java
@@ -14,3 +14,36 @@ package
 ** Дальнейшие доработки делайте сами или заказывайте у соавторов нашего проекта!
 **
 */
+  
+/**
+***
+*** Нашел класный пример! Как раз наш вариант, но надо теперь сделать переделку чтобы было видно что куда тянется.
+*** Статья вот отсюда: http://dev-blogs.com/web-service-jax-ws/
+***
+**/
+  
+package com.client;
+
+//import com.service.impl.PrintFigureService;
+//import com.service.impl.PrintFigureImplService;
+import api.uz.vtvs.bgbilling.service.impl.ContractListImplService;
+import api.uz.vtvs.bgbilling.service.impl.ContractListService;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Client {
+    public static void main(String [] args) {
+//      PrintFigureImplService figureService = new PrintFigureImplService();
+        ContractListImplService contractList = new ContractListImplService();
+//      PrintFigureService service = figureService.getPrintFigureImplPort();
+        ContractListService service = contractList.getContractListImplPort();
+        List<Integer> arguments = new ArrayList<Integer>();
+        try {
+            arguments.add(Integer.parseInt(args[1]));
+            arguments.add(Integer.parseInt(args[2]));
+            System.out.println(service.showInfo(args[0], arguments));
+        } catch (NumberFormatException exc) {
+            exc.printStackTrace();
+        }
+    }
+}
